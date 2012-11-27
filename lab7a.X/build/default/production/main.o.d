eec6361a9ff3f75fee164ae0c491f6da1cd1c154
Alex Schuldberg <Alex@Macintosh-2.local>
2012-11-26 22:47:07 -0800

    TWEAKS FOR TWEAKERS

--- lab7a.X/build/default/production/main.o.d
+++ lab7a.X/build/default/production/main.o.d
@@ -1,10 +1,5 @@
 build/default/production/main.o:  \
 main.S 
 build/default/production/main.o:  \
- /opt/microchip/xc32/v1.11/bin/../lib/gcc/pic32mx/4.5.1/../../../../pic32mx/include/cp0defs.h  \
- /opt/microchip/xc32/v1.11/bin/../lib/gcc/pic32mx/4.5.1/../../../../pic32mx/include/proc/ppic32mx.h  \
- /opt/microchip/xc32/v1.11/bin/../lib/gcc/pic32mx/4.5.1/../../../../pic32mx/include/proc/p32mx460f512l.h  \
- /opt/microchip/xc32/v1.11/bin/../lib/gcc/pic32mx/4.5.1/../../../../pic32mx/include/xc.h  \
- /opt/microchip/xc32/v1.11/bin/../lib/gcc/pic32mx/4.5.1/../../../../pic32mx/include/p32xxxx.h  \
   \
 main.S  \
